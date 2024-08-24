0d9144ab54669248b9281988dffb9977e58710c5
wyc1244 <wyc7183@gmail.com>
2024-08-24 14:29:55 +0800

    fix bug

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -60,10 +60,10 @@ const Navbar = () => (
         <div className="w-{30%} flex items-center gap-4 xl:gap-8 justify-end">
             <ClerkLoading>
                 <div
-                    class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                    className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
                     role="status">
                     <span
-                        class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                        className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
                     >Loading...</span>
                 </div>
 
