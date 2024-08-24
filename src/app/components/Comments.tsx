6f6e1b65541d4106d2d19af6bb22a463f1e97cf2
wyc1244 <wyc7183@gmail.com>
2024-08-24 14:27:00 +0800

    code fomtter

--- src/app/components/Comments.tsx
+++ src/app/components/Comments.tsx
@@ -1,19 +1,35 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const Comments = () => {
-    return (
-        <div className="">
-            {/* WRITE */}
-            <div className="flex items-center gap-4">
-                <Image src="https://images.pexels.com/photos/1261572/pexels-photo-1261572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={32} height={32} className="w-8 h-8 rounded-full" />
-                <div className="flex-1 flex items-center justify-between bg-slate-100 rounded-xl text-sm px-6 py-2 w-full">
-                    <input type="text" placeholder="Write a comment..." className="bg-transparent outline-none flex-1" />
-                    <Image src="/emoji.png" alt="" width={16} height={16} className="cursor-pointer" />
-                </div>
-            </div>
-            {/* COMMENTS */}
+  return (
+    <div className="">
+      {/* WRITE */}
+      <div className="flex items-center gap-4">
+        <Image
+          src="https://images.pexels.com/photos/1261572/pexels-photo-1261572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          width={32}
+          height={32}
+          className="w-8 h-8 rounded-full"
+        />
+        <div className="flex-1 flex items-center justify-between bg-slate-100 rounded-xl text-sm px-6 py-2 w-full">
+          <input
+            type="text"
+            placeholder="Write a comment..."
+            className="bg-transparent outline-none flex-1"
+          />
+          <Image
+            src="/emoji.png"
+            alt=""
+            width={16}
+            height={16}
+            className="cursor-pointer"
+          />
         </div>
-    )
-}
+      </div>
+      {/* COMMENTS */}
+    </div>
+  );
+};
 
-export default Comments
+export default Comments;
--- src/app/components/MobileMenu.tsx
+++ src/app/components/MobileMenu.tsx
@@ -1,32 +1,41 @@
-'use client'
-import { useState } from "react"
-import Link from "next/link"
+"use client";
+import { useState } from "react";
+import Link from "next/link";
 
 const MobileMenu = () => {
-
-    const [isOpen, setIsOpen] = useState(false)
-    return (
-        <div className='md:hidden'>
-            <div
-                className='flex flex-col gap-[4.3px] cursor-pointer' 
-                onClick={() => setIsOpen(prev => !prev)}
-            >
-                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${ isOpen ? "rotate-45" : ""} origin-left ease-in-out duration-490`}/>
-                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "opacity-0": ""} ease-in-out duration-490`}/>
-                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "-rotate-45": ""} origin-left ease-in-out duration-490`}/>
-            </div>
-            {isOpen && (
-                <div className='absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10'>
-                    <Link href="/">Home</Link>
-                    <Link href="/">Friends</Link>
-                    <Link href="/">Groups</Link>
-                    <Link href="/">Stories</Link>
-                </div>
-
-            )}
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="md:hidden">
+      <div
+        className="flex flex-col gap-[4.3px] cursor-pointer"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "rotate-45" : ""
+          } origin-left ease-in-out duration-490`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "opacity-0" : ""
+          } ease-in-out duration-490`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "-rotate-45" : ""
+          } origin-left ease-in-out duration-490`}
+        />
+      </div>
+      {isOpen && (
+        <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
+          <Link href="/">Home</Link>
+          <Link href="/">Friends</Link>
+          <Link href="/">Groups</Link>
+          <Link href="/">Stories</Link>
         </div>
-    )
-    
-}
+      )}
+    </div>
+  );
+};
 
-export default MobileMenu
+export default MobileMenu;
