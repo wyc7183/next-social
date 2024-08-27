c1c2537d4744da40d667778819a988553cc447ef
wyc1244 <wyc7183@gmail.com>
2024-08-27 21:02:22 +0800

    left bar

--- src/app/components/LeftMenu.tsx
+++ src/app/components/LeftMenu.tsx
@@ -1,7 +1,95 @@
-const LeftMenu = () =>{
-    return (
-        <div className="">LeftMenu</div>
-    )
-}
+import ProfileCard from "./ProfileCard";
+import Link from "next/link";
+import Image from "next/image";
+
+const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
+  return (
+    <div className="flex flex-col gap-6">
+      {type === "home" && <ProfileCard />}
+      <div className="p-4 bg-white rounded-lg shadow-md text-sm text-gray-500 flex flex-col gap-2">
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/posts.png" alt="" width={20} height={20} />
+          <span>My Posts</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/activity.png" alt="" width={20} height={20} />
+          <span>Activity</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/mark.png" alt="" width={20} height={20} />
+          <span>Marketplace</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/events.png" alt="" width={20} height={20} />
+          <span>Events</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/albums.png" alt="" width={20} height={20} />
+          <span>Albums</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/videos.png" alt="" width={20} height={20} />
+          <span>Videos</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/news.png" alt="" width={20} height={20} />
+          <span>News</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/courses.png" alt="" width={20} height={20} />
+          <span>Courses</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/list.png" alt="" width={20} height={20} />
+          <span>Lists</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/settings.png" alt="" width={20} height={20} />
+          <span>Settings</span>
+        </Link>
+        <br className="border-t-1 border-gray-50 w-36 self-center" />
+      </div>
+    </div>
+  );
+};
 
 export default LeftMenu;
---
+++ src/app/components/ProfileCard.tsx
@@ -0,0 +1,5 @@
+const ProfileCard = () => {
+  return <div className="">ProfileCard</div>;
+};
+
+export default ProfileCard;
